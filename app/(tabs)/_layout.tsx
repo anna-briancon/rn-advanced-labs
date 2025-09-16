c43fa71deeace644071aeb180eea7865eb3e3b8a
Anna <anna.briancon33@gmail.com>
2025-09-16 10:33:08 +0200

    feat(tp1): nav profile card

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="profileCard"
+        options={{
+          title: 'Profile Card',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
